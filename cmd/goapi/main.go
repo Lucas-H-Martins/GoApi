f5d4228cc19b9a0d91b86a1e6993abc2c3e9ba95
Lucas Henrique <lucasti@live.com>
2025-05-19 18:20:27 -0300

    removed some code

--- cmd/goapi/main.go
+++ cmd/goapi/main.go
@@ -22,24 +22,4 @@ func main() {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	defer db.Close()
-
-	// // Initialize router
-	// router := routes.NewRouter(db.GetDB())
-
-	// // Configure server
-	// addr := fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port)
-	// server := &http.Server{
-	// 	Addr:    addr,
-	// 	Handler: router,
-	// }
-
-	// // Log startup information
-	// log.Printf("Starting server in %s mode", cfg.Environment)
-	// log.Printf("Server is running on %s", addr)
-
-	// // Start server
-	// if err := server.ListenAndServe(); err != nil {
-	// 	log.Fatalf("Server failed to start: %v", err)
-	// }
 }
